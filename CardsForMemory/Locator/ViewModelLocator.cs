8946a74ee54b95d5fcf6b73cf8b654d8d443313b
MySky20164407 <215301160@qq.com>
2019-04-23 09:09:14 +0800

    add: ViewModelLocator

---
+++ CardsForMemory/Locator/ViewModelLocator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace CardsForMemory.Locator {
+    /// <summary>
+    ///     定位器
+    /// </summary>
+    public class ViewModelLocator {
+        /// <summary>
+        ///     ViewModel定位器单件
+        /// </summary>
+        public static readonly ViewModelLocator Instance =
+            new ViewModelLocator();
+
+        /// <summary>
+        ///     私有构造
+        /// </summary>
+        private ViewModelLocator() {
+            //SimpleIoc.Default.Register<>();
+        }
+
+        //public XxxViewModel XxxViewModel = SimpleIoc.Default.GetInstance<>();
+    }
+}
