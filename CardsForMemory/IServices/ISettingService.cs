ac0f3cb42a6878ac3823afe3351117138b260d89
MySky20164407 <215301160@qq.com>
2019-04-26 12:40:32 +0800

    add: ISettingService

---
+++ CardsForMemory/IServices/ISettingService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Storage;
+
+namespace CardsForMemory.IServices {
+    /// <summary>
+    ///     配置存储服务
+    /// 配置存储服务采用单件模式
+    /// 全局位置配置存储服务：SettingService.Instance
+    /// </summary>
+    public interface ISettingService {
+        /// <summary>
+        ///     初始化出厂配置
+        /// </summary>
+        void Init();
+
+        /// <summary>
+        ///     存储一项设置
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <param name="value">值</param>
+        void Save(string key, string value);
+
+        /// <summary>
+        ///     存储一组设置
+        /// </summary>
+        /// <param name="key">键</param>
+        /// <param name="compositeValue">复合值</param>
+        void Save(string key, ApplicationDataCompositeValue compositeValue);
+
+        /// <summary>
+        ///     获取一项设置
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        string Value(string key);
+
+        /// <summary>
+        ///     获取一组设置
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        ApplicationDataCompositeValue CompositeValue(string key);
+
+        /// <summary>
+        ///     还原出厂设置
+        /// </summary>
+        void RestoreSettings();
+    }
+}
---
+++ CardsForMemory/Services/SettingService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using CardsForMemory.IServices;
+
+namespace CardsForMemory.Services {
+    public class SettingService : ISettingService {
+        /// <summary>
+        ///     配置服务单件
+        /// </summary>
+        public static readonly SettingService Instance = new SettingService();
+
+        /// <summary>
+        ///     本地配置
+        /// </summary>
+        private ApplicationDataContainer _localSetting;
+
+        /// <summary>
+        ///     私有构造函数
+        /// </summary>
+        private SettingService() {
+            _localSetting = ApplicationData.Current.LocalSettings;
+        }
+
+        /// <inheritdoc />
+        public void Init() {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void Save(string key, string value) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void Save(string key, ApplicationDataCompositeValue compositeValue) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public string Value(string key) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public ApplicationDataCompositeValue CompositeValue(string key) {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void RestoreSettings() {
+            throw new NotImplementedException();
+        }
+    }
+}
